8ac5c944dce8a803a025c3a5260ab9dddd2d5c76
Lucas Matheus <49918079+samsepiol1@users.noreply.github.com>
2023-12-24 19:45:00 +0000

    feliz vespera de natal 2

---
+++ src/components/rotas/rotaKami/PageKami.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import KamiImage from './imagens/kami.png';
+import logoImage from './imagens/logo.png';
+
+
+
+const emojis = ["😊", "🏖️", "🎸", "💡", "💻"];
+const PageKami = () => {
+  const [emoji, setEmoji] = useState(emojis[0]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      let novoEmoji;
+      do {
+        novoEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+      } while (novoEmoji === emoji);
+
+      setEmoji(novoEmoji);
+    }, 2000); // Intervalo em milissegundos (neste caso, 2 segundos)
+
+    // Limpar o intervalo quando o componente é desmontado
+    return () => clearInterval(intervalId);
+  }, [emoji]); // Adicionado emoji como uma dependência para evitar warning de useEffect
+
+
+  
+
+ 
+  return (
+    <div>
+      {/* Barra Superior */}
+      <Link to="/">
+      <div className="barra-superior">
+        <div className="retangulo-branco">
+          <img src={logoImage} alt="Logo do App" />
+        </div>
+      </div>
+      </Link>
+
+      {/* Barra de Navegação */}
+      <div className="barra-navegacao">
+        <Link to="https://github.com/samsepiol1/SOS-Primeiros-Socorros">
+          Documentação
+        </Link>
+        <Link to="/#download">
+          Download
+        </Link>
+        <Link to="/#repositorio">
+          Repositório
+        </Link>
+      </div>
+
+      {/* Barra Fina */}
+      <div className="barra-fina"></div>
+
+      <div className="container-pessoa">
+        <div className="eclipsepessoa">
+          <img src={KamiImage} alt="Jadson" />
+
+          <div className="emojis-container">
+            <span className="emoji" id="emoji">{emoji}</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="sobre-pessoa">
+        <p>Olá, meu nome é Kamilla Nunes</p>
+      </div>
+
+      <div className="botoes-container">
+        <a className="botao" href="email@exemplo.com">
+          <FontAwesomeIcon icon={faEnvelope} /> 
+        </a>
+        <a className="botao" href="https://github.com/JadsonTSI" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} /> 
+        </a>
+        <a className="botao" href="https://www.instagram.com/jadson_leitao/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faInstagram} /> 
+        </a>
+      </div>
+
+      <div className="rodape">
+        {/* Conteúdo do rodapé */}
+      </div>
+    </div>
+  );
+};
+
+export default PageKami;
